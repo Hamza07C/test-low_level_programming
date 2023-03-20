f025d3b681f7cb79b1342907d61c8e84fac80489
Hamza07C <hamza7chkiri@hotmail.com>
2023-03-20 13:34:41 -0700

    commit 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main() {
-    printf("%s%s%s%s%s\n", "Programming is ", "like ", "building a ", "multilingual ", "puzzle, but the outcome is a piece of art,");
-    return 0;
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
