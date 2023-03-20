9bb95815afb5b898a3534028527b7bad8850f866
Hamza07C <hamza7chkiri@hotmail.com>
2023-03-20 10:42:26 -0700

    commit 4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print "Programming is like building a multilingual
+ * main - prints "Programming is like building a multilingual
  * puzzle, followed by a newline.
  * Return: Always 0.
  */
@@ -10,5 +10,5 @@ int main(void)
 
 {
     puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+    return(0);
 }
