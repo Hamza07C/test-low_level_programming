9ba8aa63b0958f79315d34aacc743dcc9c74fa5e
Hamza07C <hamza7chkiri@hotmail.com>
2023-03-20 15:14:57 -0700

    COMMIT A

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
-int main() {
-    printf("Size of char: %ld byte(s)\n", sizeof(char));
-    printf("Size of short: %ld byte(s)\n", sizeof(short));
-    printf("Size of int: %ld byte(s)\n", sizeof(int));
-    printf("Size of long: %ld byte(s)\n", sizeof(long));
-    printf("Size of long long: %ld byte(s)\n", sizeof(long long));
-    printf("Size of float: %ld byte(s)\n", sizeof(float));
-    printf("Size of double: %ld byte(s)\n", sizeof(double));
-    printf("Size of long double: %ld byte(s)\n", sizeof(long double));
-    return 0;
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of a int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
 }
