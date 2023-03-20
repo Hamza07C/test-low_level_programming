b0c889831d187561e476decab30e5e052cd814ab
Hamza07C <hamza7chkiri@hotmail.com>
2023-03-20 06:06:54 -0700

    commit M

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("%s%s%s%s%s\n", "Programming is ", "like ", "building a ", "multilingual ", "puzzle, but the outcome is a piece of art,");
+    return 0;
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("Size of char: %ld byte(s)\n", sizeof(char));
+    printf("Size of short: %ld byte(s)\n", sizeof(short));
+    printf("Size of int: %ld byte(s)\n", sizeof(int));
+    printf("Size of long: %ld byte(s)\n", sizeof(long));
+    printf("Size of long long: %ld byte(s)\n", sizeof(long long));
+    printf("Size of float: %ld byte(s)\n", sizeof(float));
+    printf("Size of double: %ld byte(s)\n", sizeof(double));
+    printf("Size of long double: %ld byte(s)\n", sizeof(long double));
+    return 0;
+}
