698cb9e0e34db3552ab061f3801fa307f850e9ae
Hamza07C <hamza7chkiri@hotmail.com>
2023-03-24 16:46:04 -0700

    commit 5.

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,9 @@
 #include"main.h"
+
 /**
- *more_numbers print numbers between 0 and 14.
- *print them ten times.
- *there is no return.
+ * more_numbers -  print numbers between 0 and 14
+ * Ten Times.
+ * Return:no return.
  */
 void more_numbers(void);
 {
