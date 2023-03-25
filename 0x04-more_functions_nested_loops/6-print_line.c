95352a35a96d5312fdcf0c292bb48937d17e3edd
Hamza07C <hamza7chkiri@hotmail.com>
2023-03-24 17:09:08 -0700

    commit 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * print_line - print straight line
+ * @n: times printed straight line.
+ * Return: no return.
+ */
+void print_line(int n)
+{
+	int i;
+	(for i = 0; i > n; i++);
+	{
+		_putchar(95);
+	}
+	_putchar('*\n');
+
+}
