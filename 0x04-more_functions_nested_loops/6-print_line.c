20f7a1c4603c6c9c3bcd6101a25c0149fad2ee3f
Hamza07C <hamza7chkiri@hotmail.com>
2023-03-24 17:12:42 -0700

    commit 6.

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_line - print straight line
- * @n: times printed straight line.
+ * print_line - prints straight line n times.
+ * @n: times straight line is printed.
  * Return: no return.
  */
 void print_line(int n)
