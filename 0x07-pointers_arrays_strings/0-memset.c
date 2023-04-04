997ac4cb9821ed88a8d179dfce997f556a454b5b
Hamza07C <hamza7chkiri@hotmail.com>
2023-04-03 17:18:56 -0700

    commit 4

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,9 +2,9 @@
 
 /*
  * _memset - fills memory with a constant byte,
- * @s: Memory area.
- * @b: Constant byte.
- * @n: Bytes filled.
+ * @s: memory area.
+ * @b: constant byte.
+ * @n: bytes filled.
  * Return: the pointer to dest.
  */
 char *_memset(char *s, char b, unsigned int n)
